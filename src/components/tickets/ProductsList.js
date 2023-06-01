6a61b4664db5c84007024996e45267faa217f5f4
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-06-01 15:53:03 -0400

    added useEffect to sort products alphabetically and a useEffect to show products only to employees

--- src/components/tickets/ProductsList.js
+++ src/components/tickets/ProductsList.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react"
 import "./Product.css"
-import { useNavigate } from "react-router-dom"
+
 
 
 export const ProductsList = () => {
     const [products, setProducts] = useState([])
+    const [filteredProducts, setFiltered] = useState([])
+
+    const localKandyUser = localStorage.getItem("kandy_user")
+    const kandyUserObject = JSON.parse(localKandyUser)
 
     useEffect(
         () => {
@@ -16,7 +20,32 @@ export const ProductsList = () => {
             console.log("Inital state of products") //view the Initial state of locations
         },
         [] //When this array is empty, you are observing inital componenet state
-    )
+    );
+
+    useEffect(
+        () => {
+            if (kandyUserObject.staff) {
+                //for employees
+                setFiltered(products)
+            }
+        },
+        [products]
+    );
+
+    useEffect(() => {
+        const sortedProducts = products.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1;
+            }
+            if (a.name > b.name) {
+                return 1;
+            }
+            return 0;
+        });
+        setFiltered(sortedProducts);
+    },
+        [products]
+    );
 
     return <>
 
@@ -24,7 +53,7 @@ export const ProductsList = () => {
 
         <article className="products">
             {
-                products.map(
+                filteredProducts.map(
                     (product) => {
                         return <section className="product" key={`product--${product.id}`}>
                             <header> {product.name}</header>
